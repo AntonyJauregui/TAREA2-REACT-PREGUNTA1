da541d889eb828125c50310de60a1eeaa47b630e
AntonyJauregui <139800467+AntonyJauregui@users.noreply.github.com>
2023-09-27 00:30:22 -0500

    2023.09.27

--- src/components/HookUseEffect.jsx
+++ src/components/HookUseEffect.jsx
@@ -13,24 +13,12 @@ export const HookUseEffect = () => {
     getApi();
   }, []);
 
-  const [episode, setEpisode] = useState();
-
-  useEffect(() => {
-    const getApi = async () => {
-      let response = await fetch("https://rickandmortyapi.com/api/episode");
-      let episode = await response.json();
-      setEpisode(episode.results);
-      return episode;
-    };
-    getApi();
-  }, []);
-
   console.log(personajes);
   return (
     <section className="container">
       <div className="containerPersonaje">
         {personajes &&
-          personajes.map((personaje, episode) => (
+          personajes.map((personaje) => (
             <div className="cardPersonaje" key={personaje.name}>
               <img src={personaje.image} alt="" />
               <h3>Nombre:</h3>
@@ -39,12 +27,10 @@ export const HookUseEffect = () => {
               <p>{personaje.species}</p>
               <h3>Localización:</h3>
               <p>{personaje.location.name}</p>
-              <h3>Origen:</h3>
+              <h3>First seen:</h3>
               <p>{personaje.origin.name}</p>
               <h3>Status:</h3>
               <p>{personaje.status}</p>
-              <h3>First seen in:</h3>
-              <p>{episode.name}</p>
             </div>
           ))}
       </div>
