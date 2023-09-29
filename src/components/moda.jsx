37924d3bad6e1e217ce29942d108a85db9aff950
AntonyJauregui <139800467+AntonyJauregui@users.noreply.github.com>
2023-09-28 22:04:23 -0500

    2023.09.28

--- src/components/moda.jsx
+++ src/components/moda.jsx
@@ -1,18 +1,17 @@
-export const Moda = ({ modal, setModal, personaje }) => {
-  console.log(modal);
-  return (
-    <>
-      {modal && (
-        <div className="modal">
-          <img src={personaje.image} alt="" />
-          <h1>{personaje.name}</h1>
-          <h3>{personaje.location.name}</h3>
-          <h3>{personaje.origin.name}</h3>
-          <h3>{personaje.status}</h3>
+export const Moda = ({modal, setModal, per}) => {
+    console.log(modal)
+    return (
 
-          <button onClick={() => setModal(false)}>Cerrar Modal</button>
-        </div>
-      )}
-    </>
-  );
-};
+        <>
+            {
+                modal && (
+                    <div className='modal'>
+                         <img src={per.name} alt="" />
+                        <h1>{per.name}</h1>
+                        <button onClick={() => setModal(false)}>Cerrar Modal</button>
+                    </div>
+                )
+            }
+        </>
+    )
+}
